1c0a4123b92dbdb9e8f9a97a2bcb50cdee3f8a71
vik <viveksurya033@gmail.com>
2020-04-11 21:13:06 +0530

    Authorization middleware added completely

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,1 +1,21 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const auth = async (req, res, next) => {
+    try{
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decode = jwt.verify(token, 'SecretKey');
+
+        const user = await User.findOne({_id : decoded.id, 'tokens.token' : token });
+        if(!user){
+            throw new Error({error : "Invalid Authorization token"});
+        }
+
+        req.token = token;
+        req.user = user;
+        next();
+    
+    }catch(e){
+        res.status(401).send({erroe: 'Invalid authorization'});
+    }
+}
