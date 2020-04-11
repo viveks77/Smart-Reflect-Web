e70b12c7f1d7f67372779ce2485e97300c2456ab
vik <viveksurya033@gmail.com>
2020-04-11 23:25:28 +0530

    Added User Routers

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -27,6 +27,15 @@ const userSchema = mongoose.Schema({
     }]
 });
 
+userSchema.methods.toJSON = function() {
+    const user = this;
+    const userObj =  user.toObject();
+    delete userObj.password;
+    delete userObj.tokens;
+
+    return userObj;
+}
+
 userSchema.methods.generateAuthId = async function(){
     const user = this;
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 const auth = require('../middleware/auth');
 const User = require('../models/userModel')
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
+router.post('/user/register', async (req, res) => {
     const user = new User(req.body);
     try{
         const token = await user.generateAuthId();
@@ -16,7 +15,7 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.post('/login', async (req, res) => {
+router.post('/user/login', async (req, res) => {
     try{
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateAuthId();
@@ -26,7 +25,50 @@ router.post('/login', async (req, res) => {
     }
 });
 
-router.patch('/user/update', async (req, res) => {});
+
+router.post('/user/logout', auth, async(req, res) => {
+    try{
+        const tokens = req.user.tokens.filer(token => {
+            return token != req.token
+        });
+        req.user.tokens = tokens;
+        await req.user.save();
+        res.status(200).send({msg : "logout Successfully"});
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
+
+router.get('/user/profile', auth, async(req, res) => {
+    try{
+        res.status(200).send(req.user);
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
+
+router.patch('/user/update', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    
+    try{
+        updates.forEach(update => {
+            req.user[update] = req.user[update]
+        })
+    await req.user.save();
+    res.status(200).send(req.user);
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
+
+router.delete('/user/remove', auth, async (req, res) => {
+    try{
+        await req.user.remove();
+        res.status(200).send({update : 'User Deleted Successfully'});
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
 
 
 
