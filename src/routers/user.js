1fadfdecabb939fc1e0f448950c64e38a16e15d2
vik <viveksurya033@gmail.com>
2020-04-19 15:58:45 +0530

    Changed the PATCH methods to POST & removed avatar routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -27,8 +27,10 @@ router.post('/user/login', async (req, res) => {
     try{
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateAuthId();
-        res.status(200).render('dashboard');
-        res.send({token});
+        res.status(200).render('dashboard', {
+            active : "Dashboard"
+        });
+        //res.send({token});
     }catch(e){
         res.status(400).render('login',{error : "Invalid Credentials"});
     }
@@ -48,15 +50,6 @@ router.post('/user/logout', auth, async(req, res) => {
     }
 });
 
-router.post('/user/profile/avatar', auth, upload.single('avatar'), async (req, res) => {
-    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer();
-    req.user.avatar = buffer;
-    await req.user.save();
-    res.status(200).send();
-}, (err, req,  res, next) => {
-    res.status(400).send(({error : err.message}));
-});
-
 router.get('/login', (req, res) => {
     res.render('login');
 })
@@ -73,6 +66,12 @@ router.get('/addnotice', auth, (req, res) => {
     });
 })
 
+router.get('/profile', (req, res) => {
+    res.render('profile', {
+        active : "Profile"
+    });
+})
+
 router.get('/user/profile', auth, async(req, res) => {
     try{
         res.status(200).send(req.user);
@@ -81,15 +80,7 @@ router.get('/user/profile', auth, async(req, res) => {
     }
 });
 
-router.get('/user/profile/avatar', auth, (req, res) => {
-    try{
-        res.status(200).send(req.user.avatar);
-    }catch(e){
-        res.status(400).send(e)
-    }
-});
-
-router.patch('/user/update', auth, async (req, res) => {
+router.post('/user/update', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     
     try{
@@ -103,7 +94,7 @@ router.patch('/user/update', auth, async (req, res) => {
     }
 });
 
-router.delete('/user/remove', auth, async (req, res) => {
+router.post('/user/remove', auth, async (req, res) => {
     try{
         await req.user.remove();
         res.status(200).send({update : 'User Deleted Successfully'});
