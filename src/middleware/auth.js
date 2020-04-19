74afde4f3d8cde5726c1876d434cbb48b348722b
vik <viveksurya033@gmail.com>
2020-04-20 00:30:04 +0530

    Modified authorization. Now uses HTTPcookies instead of header bearer

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,20 +3,20 @@ const User = require('../models/userModel');
 
 const auth = async (req, res, next) => {
     try{
-        const token = req.header('Authorization').replace('Bearer ', '');
+        
+        const token = req.cookies.token;
         const decode = jwt.verify(token, 'SecretKey');
 
         const user = await User.findOne({_id : decode.id, 'tokens.token' : token });
         if(!user){
             throw new Error({error : "Invalid Authorization token"});
         }
-
         req.token = token;
         req.user = user;
         next();
-    
+
     }catch(e){
-        res.status(401).send({erroe: 'Invalid authorization'});
+        res.status(401).render('login');
     }
 }
 
